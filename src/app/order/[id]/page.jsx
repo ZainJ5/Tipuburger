ccfd75d61d41f291a8362a247418f5d912697679
Zain Jamshaid <zainjamshaid25@gmail.com>
2025-11-13 19:46:05 +0500

    Updated whatsapp number

--- src/app/order/[id]/page.jsx
+++ src/app/order/[id]/page.jsx
@@ -901,7 +901,7 @@ export default function OrderDetailPage() {
           
           <div className="flex space-x-3">
 <a
-  href="https://api.whatsapp.com/send/?phone=923122754064"
+  href="https://api.whatsapp.com/send/?phone=923463332682"
   target="_blank"
   rel="noopener noreferrer"
   className="inline-flex items-center px-3 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
